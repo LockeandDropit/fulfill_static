7387e8a652cfd11058d3fd98fe5d7327fd19e929
Sven Olafsen <stockburner9@gmail.com>
2023-08-10 12:52:06 -0500

    add privacy

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes} from "react-router-dom"
 import Landing from './pages/Landing'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Privacy from './pages/Privacy'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
     <Route path="/" element={<Landing />} />
     <Route path="/about" element={<About />}/>
     <Route path="/contact" element={<Contact />}/>
+    <Route path="/privacy" element={<Privacy />}/>
    </Routes>
   )
 }
